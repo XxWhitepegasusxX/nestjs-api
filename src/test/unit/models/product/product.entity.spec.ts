0014552663de7e97e530a07b155a41c428abf4a6
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-13 19:56:58 -0300

    tests running with paths

---
+++ src/test/unit/models/product/product.entity.spec.ts
@@ -0,0 +1,16 @@
+import { Product } from "@models/productModel/product.entity"
+
+describe('Product test', () => {
+    it('should create a product', () => {
+        const product = new Product({
+            id: 1,
+            price: 24.90,
+            name: 'bebida',
+            image: 'image',
+            description: 'description',
+            categories: ['category'],
+        });
+        expect(product.id).toBe(1);
+        expect(product.name).toBe('bebida');
+    })
+})
