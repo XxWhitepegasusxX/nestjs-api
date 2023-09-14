46f1cac5ba652052c8d467c0ba86d4ecfcb2a7d2
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-14 10:36:36 -0300

    category controller implemented

--- src/controllers/category/category.controller.ts
+++ src/controllers/category/category.controller.ts
@@ -1,34 +1,35 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CategoryService } from '../../services/category/category.service';
-import { CreateCategoryDto } from '../../dtos/category/create-category.dto';
-import { UpdateCategoryDto } from '../../dtos/category/update-category.dto';
+
+import { CreateCategoryDto } from '@dtos/category/create-category.dto';
+import { UpdateCategoryDto } from '@dtos/category/update-category.dto';
+import { CategoryService } from '@services/category/category.service';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
-  create(@Body() createCategoryDto: CreateCategoryDto) {
-    return this.categoryService.create(createCategoryDto);
+  async create(@Body() createCategoryDto: CreateCategoryDto) {
+    return await this.categoryService.create(createCategoryDto);
   }
 
   @Get()
-  findAll() {
-    return this.categoryService.findAll();
+  async findAll() {
+    return await this.categoryService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.categoryService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    return await this.categoryService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-    return this.categoryService.update(+id, updateCategoryDto);
+  async update(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
+    return await this.categoryService.update(id, updateCategoryDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.categoryService.remove(+id);
+  async remove(@Param('id') id: string) {
+    return await this.categoryService.remove(id);
   }
 }
