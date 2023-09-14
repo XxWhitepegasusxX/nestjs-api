21f4d2a46228e77c6e44eb9d17704de3c7adf7b4
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-14 01:46:42 -0300

    prisma provider added

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from '../../services/productService/product.service';
 import { ProductController } from '../../controllers/productController/product.controller';
+import { PrismaService } from '@services/prismaService/prisma.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, PrismaService],
 })
 export class ProductModule {}
