2cbee1dd3c665d5d30afb2df8ddf2284161ea948
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-14 01:45:08 -0300

    products crud implemented

--- src/services/productService/product.service.ts
+++ src/services/productService/product.service.ts
@@ -1,26 +1,78 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from '../../dtos/productDto/create-product.dto';
-import { UpdateProductDto } from '../../dtos/productDto/update-product.dto';
+import { Product } from '@prisma/client';
+
+import { AppError } from '@errors/AppError';
+import { PrismaService } from '@services/prismaService/prisma.service';
+import { CreateProductDto } from '@dtos/productDto/create-product.dto';
+import { UpdateProductDto } from '@dtos/productDto/update-product.dto';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private prisma: PrismaService){}
+  async create(createProductDto: CreateProductDto): Promise<Product> {
+    try{
+      const product = await this.prisma.product.create({
+        data: createProductDto,
+      })
+      return product
+    }catch(e){
+      throw new AppError("Error while creating product: " + e.message)
+    }
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    try{
+      const products = await this.prisma.product.findMany();
+      return products
+    }catch(e){
+      throw new AppError("Error while querying for all products: " + e.message)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findById(id: string) {
+    try{
+      const product = await this.prisma.product.findUnique({
+        where: {id}
+      })
+      return product
+    }catch(e){
+      throw new AppError("Error while querying for product: " + e.message)
+    }
+  }
+  
+  async findByName(name: string) {
+    try{
+      const product = await this.prisma.product.findUnique({
+        where: {name}
+      })
+      return product
+    }catch(e){
+      throw new AppError("Error while querying for product: " + e.message)
+    }
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto): Promise<Product> {
+    try{
+      const updatedProduct = await this.prisma.product.update({
+        where: { id },
+        data: updateProductDto
+      })
+      return updatedProduct
+    }catch(e){
+      throw new AppError("Error updating product: " + e.message);
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string): Promise<Product> {
+    try{
+      const deletedProduct = await this.prisma.product.delete({
+        where: {
+          id
+        }
+      })
+      return deletedProduct;
+    }catch(e){
+      throw new AppError("Error removing product: " + e.message);
+    }
   }
 }
