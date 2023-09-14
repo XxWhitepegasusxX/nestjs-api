7e7ff3c3ba1756f90550168145173ada50c7781e
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-14 10:52:33 -0300

    product dto image removed

--- src/dtos/product/create-product.dto.ts
+++ src/dtos/product/create-product.dto.ts
@@ -3,7 +3,6 @@ import { Prisma } from "@prisma/client";
 export class CreateProductDto implements Prisma.ProductCreateInput {
     category?: Prisma.CategoryCreateNestedManyWithoutProductsInput;
     menu?: Prisma.MenuCreateNestedManyWithoutProductsInput;
-    image?: string;
     name: string;
     description: string;
     price: number;
--- src/dtos/product/update-product.dto.ts
+++ src/dtos/product/update-product.dto.ts
@@ -3,7 +3,6 @@ import { Prisma } from '@prisma/client';
 export class UpdateProductDto implements Prisma.ProductUpdateInput {
     name?: Prisma.StringFieldUpdateOperationsInput | string;
     price?: Prisma.FloatFieldUpdateOperationsInput | number;
-    image?: Prisma.StringFieldUpdateOperationsInput | string;
     description?: Prisma.StringFieldUpdateOperationsInput | string;
     category?: Prisma.CategoryUpdateManyWithoutProductsNestedInput;
     menu?: Prisma.MenuUpdateManyWithoutProductsNestedInput;
