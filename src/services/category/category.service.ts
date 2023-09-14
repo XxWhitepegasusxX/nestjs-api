52d1d0fedb7fac434efd01ce4d7a5db1837a2162
XxWhitepegasusxX <alexanderalmeida3040@gmail.com>
2023-09-14 10:37:53 -0300

    category service implemented

--- src/services/category/category.service.ts
+++ src/services/category/category.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { Category } from '@prisma/client';
+
 import { CreateCategoryDto } from '@dtos/category/create-category.dto';
 import { UpdateCategoryDto } from '@dtos/category/update-category.dto';
 import { PrismaService } from '@services/prisma/prisma.service';
 import { AppError } from '@errors/AppError';
-import { Category } from '@prisma/client';
 
 @Injectable()
 export class CategoryService {
@@ -20,33 +21,39 @@ export class CategoryService {
 
   async findAll(): Promise<Category[]> {
     try{
-      return await this.prisma.category.findMany()
+      return await this.prisma.category.findMany({include: {products: true}})
     }catch(e){
       throw new AppError("Error getting categories: " + e.message)
     }
   }
 
-  async findOne(id: number) {
+  async findOne(id: string): Promise<Category> {
     try{
-
+      return await this.prisma.category.findUnique({where: {id}, include: {products: true}})
     }catch(e){
-      throw new AppError("Error creating category: " + e.message)
+      throw new AppError("Error finding category: " + e.message)
     }
   }
 
-  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  async update(id: string, updateCategoryDto: UpdateCategoryDto): Promise<Category>{
     try{
-
+      return await this.prisma.category.update({
+        where: {id},
+        data: updateCategoryDto,
+        include: {
+          products: true
+        }
+      })
     }catch(e){
-      throw new AppError("Error creating category: " + e.message)
+      throw new AppError("Error updating category: " + e.message)
     }
   }
 
-  async remove(id: number) {
+  async remove(id: string) {
     try{
-
+      return await this.prisma.category.delete({where: {id}})
     }catch(e){
-      throw new AppError("Error creating category: " + e.message)
+      throw new AppError("Error removing category: " + e.message)
     }
   }
 }
